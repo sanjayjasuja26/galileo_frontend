8a533bf27d271d606dcf3da260b99baa79ab664d
sanam.k <sanam.k@iapptechnologies.com>
2022-04-22 15:30:05 +0530

    new pagination component

--- src/components/Access/CaseAccess.js
+++ src/components/Access/CaseAccess.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Loader from "../Common/Loader";
-// import NumericPagination from "../Pagination/Pagination";
 import CustomPagination from "../Pagination/CustomPagination";
 import AllCasesTable from "../Tables/AllCasesTable";
 
@@ -10,15 +9,14 @@ const CaseAccess = () => {
   const { user, access } = useSelector(state => state.auth);
   const { cases: { loading, error, data, total } } = useSelector(state => state.cases);
 
-  if(loading){
+  if(loading){   
     return <div className="text-center"><Loader /></div> 
   } else if(data?.length > 0){
-    return <>   
+    return <>         
     <AllCasesTable cases={data} />
       {
         (total === 0 || access === 'P' || (access === 'Y' && !user.verify)) 
         ? null : 
-        // <NumericPagination />
         <CustomPagination />
       }
     </>
